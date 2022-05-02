74d73d0aab69af45a0042ef55806256497d2d55b
seanm11 <seanm5@umbc.edu>
2022-05-02 14:49:32 -0400

    W

--- umbc59/config.h
+++ umbc59/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID 0x0000 
 #define DEVICE_VER 0x0001
 #define MANUFACTURER MKBD
-#define PRODUCT MKBC109
+#define PRODUCT MKBC59
 
 /* key matrix size */
 #define MATRIX_ROWS 5
